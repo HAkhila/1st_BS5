506e73b68c98294e4d76cf55f561c3c99b89b9f2
Akhila H <Akhilah@er.ai>
2022-11-03 18:26:00 +0530

    Boostrap

--- src/App.js
+++ src/App.js
@@ -1,22 +1,52 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import  { Container, Row, Col , Button , Alert, Card, Form}  from 'react-bootstrap';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Container fluid>
+        <Form>
+         <Row>
+          <Col md>
+          <Form.Group controlId='formEmail'>
+            <Form.Label>E.mail Address</Form.Label>
+            <Form.Control type="email" placeholder='Example@email.com'/>
+            <Form.Text className='text-muted'>
+              Enter vaild email Address
+            </Form.Text>
+            </Form.Group>
+          </Col>
+         
+         <Col md>
+          <Form.Group controlId='Password'>
+          <Form.Label>Password</Form.Label>
+            <Form.Control type="Password" placeholder="Password"/>
+          </Form.Group>
+          </Col>
+         </Row>
+          <Button variant= "secondary" type="submit">Login</Button>
+        </Form>
+
+        <Card  style= {{color: "#000"} }>
+          <Card.Img src= "990.jpg" />
+          <Card.Body>
+           <Card.Title>
+           Chennakesava Temple
+           </Card.Title>
+           <Card.Text >
+          
+            </Card.Text> 
+            <Button variant= "primary">Read more</Button>
+          </Card.Body>
+        </Card>
+        
+        <Alert variant= "success">This is a button</Alert>
+        <Button>Test Button</Button>
+        </Container>
       </header>
     </div>
   );
